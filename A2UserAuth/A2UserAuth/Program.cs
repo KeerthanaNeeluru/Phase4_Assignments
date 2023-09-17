d8178fc988e08e60a40b17348c739b11f7faca08
keerthana <keerthananeeluru2001@gmail.com>
2023-09-17 10:54:04 +0530

    Day2

---
+++ A2UserAuth/A2UserAuth/Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace A2UserAuth
+{
+    internal class UserAuthenticator
+    {
+
+        public string Uname { get; set; }
+        public string Upwd { get; set; }
+
+        public UserAuthenticator()
+        {
+            Uname = null;
+            Upwd = null;
+        }
+        public bool RegUser(string uname, string pwd)
+        {
+            if (Uname == null)
+            {
+                Uname = uname;
+                Upwd = pwd;
+                return true;
+            }
+            return false;
+        }
+        public bool logUser(string uname, string pwd)
+        {
+            if (Uname == uname || Upwd == pwd)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool ResetPwd(string uname, string pwd)
+        {
+            if (Uname == uname)
+            {
+                Upwd = pwd;
+                return true;
+            }
+            return false;
+        }
+    }
+    internal class program 
+    {
+        
+    
+    static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ A2UserAuth/A2UserAuth/UserAuthTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2UserAuth
+{
+    [TestFixture]
+    internal class UserAuthTest
+    {
+        public UserAuthenticator auth;
+        [SetUp]
+        public void Setup()
+        {
+            auth=new UserAuthenticator();
+        }
+        [Test]
+        public void TestUserReg()
+        {
+            Assert.IsTrue(auth.RegUser("user1", "pwd1"));
+            Assert.IsFalse(auth.RegUser("user1", "pwd2"));
+        }
+        [Test]
+        public void TestUserLog()
+        {
+            auth.RegUser("user3", "pwd3");
+            Assert.IsTrue(auth.logUser("user3", "pwd3"));
+          
+            Assert.IsFalse(auth.logUser("nonexistentuser", "password"));
+        }
+        [Test]
+        public void TestPwdReset()
+        {
+            auth.RegUser("user4", "pwd4");
+            Assert.IsTrue(auth.ResetPwd("user4", "newpwd4"));
+            Assert.IsFalse(auth.ResetPwd("userNotExist", "newpwd"));
+        }
+    }
+}
